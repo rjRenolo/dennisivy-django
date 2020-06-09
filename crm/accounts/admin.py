ec93d87ddf67a1ecb80b0b726ca7abb51ab5a354
rjRenolo <rjrenolo14@gmail.com>
2020-06-09 10:46:02 +0800

    model relationships

--- crm/accounts/admin.py
+++ crm/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Costumer, Product, Order, Tag
+
 # Register your models here.
+
+admin.site.register(Costumer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Tag)
